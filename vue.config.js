e509272580b059c46c0a39653873f89ddcdf71eb
Omar Siam <Omar.Siam@oeaw.ac.at>
2021-02-26 01:23:52 +0100

    We want the tags+commit hash if necessary string as version.

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,9 @@ const GitRevisionPlugin = require('git-revision-webpack-plugin')
 module.exports = {
   'chainWebpack': config => {
     config.plugin('define').tap(args => {
-      const gitRevisionPlugin = new GitRevisionPlugin()
+      const gitRevisionPlugin = new GitRevisionPlugin({
+        lightweightTags: true
+      })
       args[0]['process.env']['VUE_APP_VERSION'] = JSON.stringify(gitRevisionPlugin.version())
       args[0]['process.env']['VUE_APP_COMMIT_HASH'] = JSON.stringify(gitRevisionPlugin.commithash())
       args[0]['process.env']['VUE_APP_BRANCH'] = JSON.stringify(gitRevisionPlugin.branch())

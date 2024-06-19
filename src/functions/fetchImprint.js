b81d206bfd473f82279e313fae7af953af12a1c0
Omar Siam <Omar.Siam@oeaw.ac.at>
2024-06-19 15:03:01 +0200

    Switch to new imprint service

--- src/functions/fetchImprint.js
+++ src/functions/fetchImprint.js
@@ -4,7 +4,7 @@ const sax = require('sax')
 
 const localFunctions = {
     getImprintHTML: async function(id, addClasses) {
-        const data = await httpGet(`https://shared.acdh.oeaw.ac.at/acdh-common-assets/api/imprint.php?serviceID=${id}`, (rawData, onlyAttr, val, onlyTopTag) => {processResult(rawData, onlyAttr, val, onlyTopTag, addClasses)})
+        const data = await httpGet(`https://imprint.acdh.oeaw.ac.at/${id}/?fromat=html`, (rawData, onlyAttr, val, onlyTopTag) => {processResult(rawData, onlyAttr, val, onlyTopTag, addClasses)})
         return data
     }
 }
@@ -37,7 +37,7 @@ return new Promise ((resolve, reject) => {
   res.on('data', (chunk) => { rawData += chunk; });
   res.on('end', () => {
     try {
-      resolve(processResult(rawData, 'lang', 'en', 'div'));
+      resolve(rawData);
     } catch (e) {
       console.error(e.message);
       reject(e)
@@ -48,62 +48,6 @@ return new Promise ((resolve, reject) => {
   reject(e)
 });
 })
-}
-
-function processResult(rawData, onlyAttr, val, onlyTopTag, addClasses) {
-  const parser = sax.parser(false, {lowercase: true})
-  let res = ''
-  let tagname = []
-  let ignore = false
-  let isSelfClosing = false
-  parser.onerror = function (e) {
-    throw new Error(e)
-  };
-  parser.ontext = function (t) {
-    if (ignore) {return}
-    res += t
-  };
-  parser.onopentag = function (node) {
-    isSelfClosing = node.isSelfClosing
-    ignore = (node.attributes[onlyAttr] && node.attributes[onlyAttr] !== val) ||
-             (tagname.length === 1 && node.name !== onlyTopTag) ? `${node.name}_${tagname.length}` : ignore
-    tagname.push(node.name)
-    if (ignore || node.name === '____') {return}
-    if (isSelfClosing) {
-      res += `<${node.name}${serializeAttr(node.attributes)}/>`
-    } else if (tagname.length === 1) {
-      if (node.attributes.class) {
-        res += `<${node.name}${serializeAttr(node.attributes, addClasses)}>`
-      } else {
-        res += `<${node.name}${serializeAttr(node.attributes)} class="${addClasses}">`
-      }
-    } else {
-      res += `<${node.name}${serializeAttr(node.attributes)}>`
-    }
-  };
-  parser.onclosetag = function () {
-    const nodeName = tagname.pop(),
-          endIgnore = ignore || isSelfClosing
-    ignore = ignore === `${nodeName}_${tagname.length}` ? false : ignore
-    isSelfClosing = false
-    if (endIgnore || nodeName === '____') {return}
-    res += `</${nodeName}>`
-  };
-  
-  parser.write(`<____>${rawData}</____>`).close()
-  return res
-}
-
-function serializeAttr(attributes, addClasses) {
-  let res = ''
-  for (let attr of Object.keys(attributes)) {
-    if (attr == 'class' && addClasses) {
-      res += ` ${attr}="${attributes[attr]} ${addClasses}"`
-    } else {
-      res += ` ${attr}="${attributes[attr]}"`
-    }
-  }
-  return res
 }
 
 module.exports = localFunctions.getImprintHTML
